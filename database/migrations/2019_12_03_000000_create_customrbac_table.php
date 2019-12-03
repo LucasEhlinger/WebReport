3852d3cbd77782283d91311f8e5d72a62a5149d9
Lucas Ehlinger <lucas.ehlinger@gmail.com>
2019-12-03 10:45:06 +0100

    fix laravel syntax error

--- database/migrations/2019_12_03_000000_create_customrbac_table.php
+++ database/migrations/2019_12_03_000000_create_customrbac_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCustomrbacTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,49 +13,37 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-            /*customs*/
-            $table->int('status');
-        });
-
         Schema::create('command', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->int('resource_type'); // roles, projects, lot, activities.
+            $table->integer('resource_type'); // roles, projects, lot, activities.
             $table->string('command_name'); // read, write, create
         });
 
         Schema::create('role', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->int('parent_id')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
         });
 
         Schema::create('role_command', function (Blueprint $table) {
-            $table->int('user_id');
-            $table->int('role_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('role_id')->references('id')->on('role');
         });
 
         Schema::create('user_project_role', function (Blueprint $table) {
-            $table->int('user_id');
-            $table->int('project_id');
-            $table->int('role_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('role_id');
 
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('role_id')->references('id')->on('role');
 
             // todo
-            if (Schema::hasTable('project')) {
+            /*if (Schema::hasTable('project')) {
                 $table->foreign('project_id')->references('id')->on('project');
-            }
+            }*/
         });
     }
 
